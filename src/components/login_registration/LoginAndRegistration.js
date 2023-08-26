78ab723604c7df78ec6cc2ca6c9049e232d71dfb
Vijender Erra <vijendererra@gmail.com>
2023-08-26 23:41:25 +0530

    added responsive

--- src/components/login_registration/LoginAndRegistration.js
+++ src/components/login_registration/LoginAndRegistration.js
@@ -35,118 +35,124 @@ const LoginAndRegistration = () => {
   }
 
   return (
-    <Form form={form} layout="vertical" >
-      <Row>
-        <Col span={15}>
-          <img src={img} className="img" />
-        </Col>
-        <Col span={9}>
+    <div className="split-container">
+      <div className="left-panel">
+        <img src={img} className="img" />
+      </div>
+      <div className="right-panel">
+        <Form form={form} layout="vertical" >
           <Row>
-            <Col span={5}></Col>
-            <Col span={19}>
-              <h4 className="card-title">Institute Management Registration</h4>
-            </Col>
-            <Col span={11}>
-              <Form.Item label="First Name" name="firstName" rules={[{ required: true, message: 'Please enter your first name' }]}>
-                <Input placeholder="First Name" />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={11}>
-              <Form.Item label="Middle Name" name="middleName">
-                <Input placeholder="Middle Name" />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item label="Last Name" name="lastName" rules={[{ required: true, message: 'Please enter your last name' }]}>
-                <Input placeholder="Last Name" />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={11}>
-              <Form.Item label="Email" name="email" rules={[
-                { required: true, message: 'Please enter your email' },
-                { type: 'email', message: 'Please enter a valid email' },
-              ]}>
-                <Input prefix={<UserOutlined />} placeholder="Email" />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item label="Password" name="password" rules={[
-                { required: true, message: 'Please enter your password' },
-                { min: 8, message: 'Password must be at least 8 characters long' }
-              ]}>
-                <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={11}>
-              <Form.Item label="Confirm Password" name="confirmPassword" rules={[
-                { required: true, message: 'Please enter your Confirm Password' },
-                { validator: validatePasswordConfirm }
-              ]}>
-                <Input.Password prefix={<LockOutlined />} placeholder="Confirm Password" />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item label="Contact Number" name="contactNumber" rules={[
-                { required: true, message: 'Please enter your contact number' },
-                { validator: validatePhoneNumber }
-              ]}>
-                <InputNumber
-                  style={{ width: '100%' }}
-                  placeholder="Phone Number"
-                  prefix={<PhoneOutlined />} />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={11}>
-              <Form.Item label="Institute Name" name="instituteName" rules={[
-                { required: true, message: 'Please enter your contact number' },
-              ]}>
-                <Input placeholder="Institute Name" />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item label="Country" name="country" rules={[
-                { required: true, message: 'Please enter your country' },
-              ]}>
-                <Input placeholder="Country" />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={11}>
-              <Form.Item label="State" name="state" rules={[
-                { required: true, message: 'Please enter your state' },
-              ]}>
-                <Input placeholder="State" />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item label="City" name="city" rules={[
-                { required: true, message: 'Please enter your city' },
-              ]}>
-                <Input placeholder="City" />
-              </Form.Item>
-            </Col>
-            <Col span={1}></Col>
-            <Col span={11}>
-              <Form.Item label="Zip Code" name="zipCode" rules={[
-                { required: true, message: 'Please enter your zip coder' },
-              ]}>
-                <Input placeholder="Zip Code" />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item>
-                <Button type="primary" onClick={reSetForm}>Reset</Button> &nbsp;
-                <Button type="primary" onClick={register}>Register</Button>
-              </Form.Item>
+            <Col xs={2} sm={2} md={2} lg={1}></Col>
+            <Col xs={22} sm={22} md={22} lg={23}>
+              <Row>
+                <Col xs={24} sm={16} md={12} lg={5}></Col>
+                <Col xs={24} sm={16} md={12} lg={19}>
+                  <h4 className="card-title">Institute Management Registration</h4>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="First Name" name="firstName" rules={[{ required: true, message: 'Please enter your first name' }]}>
+                    <Input placeholder="First Name" />
+                  </Form.Item>
+                </Col>
+                <Col xs={0} sm={0} md={0} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Middle Name" name="middleName">
+                    <Input placeholder="Middle Name" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Last Name" name="lastName" rules={[{ required: true, message: 'Please enter your last name' }]}>
+                    <Input placeholder="Last Name" />
+                  </Form.Item>
+                </Col>
+                <Col xs={0} sm={0} md={0} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Email" name="email" rules={[
+                    { required: true, message: 'Please enter your email' },
+                    { type: 'email', message: 'Please enter a valid email' },
+                  ]}>
+                    <Input prefix={<UserOutlined />} placeholder="Email" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Password" name="password" rules={[
+                    { required: true, message: 'Please enter your password' },
+                    { min: 8, message: 'Password must be at least 8 characters long' }
+                  ]}>
+                    <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+                  </Form.Item>
+                </Col>
+                <Col xs={0} sm={0} md={0} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Confirm Password" name="confirmPassword" rules={[
+                    { required: true, message: 'Please enter your Confirm Password' },
+                    { validator: validatePasswordConfirm }
+                  ]}>
+                    <Input.Password prefix={<LockOutlined />} placeholder="Confirm Password" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Contact Number" name="contactNumber" rules={[
+                    { required: true, message: 'Please enter your contact number' },
+                    { validator: validatePhoneNumber }
+                  ]}>
+                    <InputNumber
+                      style={{ width: '100%' }}
+                      placeholder="Phone Number"
+                      prefix={<PhoneOutlined />} />
+                  </Form.Item>
+                </Col>
+                <Col xs={0} sm={0} md={0} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Institute Name" name="instituteName" rules={[
+                    { required: true, message: 'Please enter your contact number' },
+                  ]}>
+                    <Input placeholder="Institute Name" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Country" name="country" rules={[
+                    { required: true, message: 'Please enter your country' },
+                  ]}>
+                    <Input placeholder="Country" />
+                  </Form.Item>
+                </Col>
+                <Col xs={0} sm={0} md={0} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="State" name="state" rules={[
+                    { required: true, message: 'Please enter your state' },
+                  ]}>
+                    <Input placeholder="State" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="City" name="city" rules={[
+                    { required: true, message: 'Please enter your city' },
+                  ]}>
+                    <Input placeholder="City" />
+                  </Form.Item>
+                </Col>
+                <Col xs={0} sm={0} md={0} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={11}>
+                  <Form.Item label="Zip Code" name="zipCode" rules={[
+                    { required: true, message: 'Please enter your zip coder' },
+                  ]}>
+                    <Input placeholder="Zip Code" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24} sm={16} md={12} lg={3}>
+                  <Button type="primary" onClick={reSetForm}>Reset</Button>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={1}></Col>
+                <Col xs={24} sm={16} md={12} lg={6}>
+                  <Button type="primary" onClick={register}>Register</Button>
+                </Col>
+              </Row>
             </Col>
           </Row>
-        </Col>
-      </Row>
-    </Form >
+        </Form >
+      </div>
+    </div>
   );
 
 }
